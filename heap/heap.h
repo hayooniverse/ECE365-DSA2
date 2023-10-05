b23c17a8ceb4ac5cd652325ca1afc4f15bf8ae77
hayooniverse <115905655+hayooniverse@users.noreply.github.com>
2023-10-05 07:36:14 -0400

    Update heap.h

---
+++ heap/heap.h
@@ -0,0 +1,51 @@
+// Hailey Hayoon Chung
+
+#ifndef _HEAP_H
+#define _HEAP_H
+
+#include <vector>
+#include <string>
+
+class heap{
+    public:
+        // The constructor allocates space for the nodes of the heap and the mapping (hash table) basd on the specified capacity
+        
+        heap(int capacity);
+
+        // Inserts a new node into the binary heap
+        // Inserts a node with the specified id string, key, and optionally a pointer. The key is used to determine the final position of the new node.
+        int insert(const std::string &id, int key, void *pv = nullptr);
+
+        // setKey - set the key of the specified node to the specified value
+        int setKey(const std::string &id, int key);
+
+        // deleteMin - return the data associated with the smallest key and delete that node from the binary heap
+        int deleteMin(std:: string *pId = nullptr, int *pKey = nullptr, void *ppData = nullptr);
+
+        // remove - delete the node with the specified id from the binary heap
+        int remove(const std::string &id, int *pKey = nullptr, void *ppData = nullptr);
+    
+    private:
+        // The capacity of the heap
+        int capacity;
+        // The current size of the heap
+        int currentSize;
+
+        // "node" - a private nested class within the heap class
+        class node{
+            public:
+                std::string id;
+                int key;
+                void *pv;
+
+                // The constructor for node
+                node(const std::string &id, int key, void *pv) : id(id), key(key), pv(pv) {}
+            
+        }
+
+        // The vector of node objects containing the actual data of the heap
+        std::vector<node> data;
+
+}
+
+#endif //_HEAP_H
